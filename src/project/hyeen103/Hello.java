9f8b350adedcb29e46eadf8cdaf8e4462337c73f
김지현 <hyeen103@gmail.com>
2024-09-23 15:02:27 +0900

    [UPDATE]

---
+++ src/project/hyeen103/Hello.java
@@ -0,0 +1,36 @@
+/**
+ * YOU ARE STRICTLY PROHIBITED TO COPY, DISCLOSE, DISTRIBUTE, MODIFY OR USE THIS PROGRAM
+ * IN PART OR AS A WHOLE WITHOUT THE PRIOR WRITTEN CONSENT OF HIMEDIA.CO.KR.
+ * HIMEDIA.CO.KR OWNS THE INTELLECTUAL PROPERTY RIGHTS IN AND TO THIS PROGRAM.
+ * COPYRIGHT (C) 2024 HIMEDIA.CO.KR ALL RIGHTS RESERVED.
+ *
+ * 하기 프로그램에 대한 저작권을 포함한 지적재산권은 himedia.co.kr에 있으며,
+ * himedia.co.kr이 명시적으로 허용하지 않는 사용, 복사, 변경 및 제 3자에 의한 공개, 배포는 엄격히 금지되며
+ * himedia.co.kr의 지적재산권 침해에 해당된다.
+ * Copyright (C) 2024 himedia.co.kr All Rights Reserved.
+ *
+ *
+ * Program		: kr.co.himedia.ecommerce
+ * Description	:
+ * Environment	: JRE 1.7 or more
+ * File			: Hello.java
+ * Notes		:
+ * History		: [NO][Programmer][Description]
+ *				: [20240923142120][hyeen103#gmail.com][CREATE: Initial Release]
+ */
+package project.hyeen103;
+
+/**
+ * @version 1.0.0
+ * @author hyeen103@gmail.com
+ * 
+ * @since 2024-09-23
+ * <p>DESCRIPTION:</p>
+ * <p>IMPORTANT:</p>
+ */
+public class Hello {
+
+	public static void main(String[]args) {
+		System.out.println("Hello World!");
+	}
+}
